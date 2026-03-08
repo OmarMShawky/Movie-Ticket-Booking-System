42b0b824fb8e38ff644e9560f4189836dc567032
OmarMShawky <oshawky96@outlook.com>
2026-03-08 18:47:24 +0200

    Assignment 2 Part 2

---
+++ AssignmentOneOOP/BookingHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentOneOOP;
+
+static class BookingHelper
+{
+    private static int _bookingCounter = 0;
+
+    public static double CalcGroupDiscount(int numberOfTickets, double pricePerTicket)
+    {
+        double total = numberOfTickets * pricePerTicket;
+
+        if (numberOfTickets >= 5)
+            return total * 0.90; // 10% discount
+
+        return total;
+    }
+    public static string GenerateBookingReference()
+    {
+        _bookingCounter++;
+        return $"BK-{_bookingCounter}";
+    }
+}
---
+++ AssignmentOneOOP/Cinema.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentOneOOP;
+
+class Cinema
+{
+    private Ticket[] _tickets = new Ticket[20];
+
+    // a. Index-based get/set
+    public Ticket this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= _tickets.Length)
+                return null;
+            return _tickets[index];
+        }
+        set
+        {
+            if (index < 0 || index >= _tickets.Length)
+                return;
+            _tickets[index] = value;
+        }
+    }
+
+    // b. Get ticket by movie name
+    public Ticket GetMovieByName(string movieName)
+    {
+        foreach (Ticket ticket in _tickets)
+        {
+            if (ticket != null && ticket.MovieName == movieName)
+                return ticket;
+        }
+        return null;
+    }
+
+    // c. Add ticket to first available slot
+    public bool AddTicket(Ticket t)
+    {
+        for (int i = 0; i < _tickets.Length; i++)
+        {
+            if (_tickets[i] == null)
+            {
+                _tickets[i] = t;
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int TotalTicketsSold()
+    {
+        return Ticket.TicketCounter; // reads the static counter from Ticket class
+    }
+}
